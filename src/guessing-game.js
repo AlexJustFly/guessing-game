cbee17798cf83936a0adb75593548a11e8223fcb
AlexJustFly <alexjustfly@gmail.com>
2019-10-06 10:54:48 +0300

    add 100% solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.cent = Math.round( (this.min + this.max) / 2 );
+        return this.cent;
     }
 
     lower() {
-
+        this.max=this.cent;
     }
 
     greater() {
-
+        this.min=this.cent;
     }
 }
 
